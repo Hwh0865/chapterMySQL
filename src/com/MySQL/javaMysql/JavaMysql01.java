7ef7c62850bf39ab6dd58862841f671a3c246803
Hwh <huwenhao0865@163.com>
2022-06-08 18:04:39 +0800

    first commit

---
+++ src/com/MySQL/javaMysql/JavaMysql01.java
@@ -0,0 +1,8 @@
+package com.MySQL.javaMysql;
+
+public class JavaMysql01 {
+    public static void main(String[] args) throws ClassNotFoundException {
+//        Class.forName("com.MySQL.javaMysql.Driver");
+        System.out.println("hello.git");
+    }
+}
