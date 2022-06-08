9c9b45347af0dca489313a0b3deb5c8882b439e7
Hwh <huwenhao0865@163.com>
2022-06-08 18:25:00 +0800

    push commit2

--- src/com/MySQL/javaMysql/JavaMysql01.java
+++ src/com/MySQL/javaMysql/JavaMysql01.java
@@ -7,7 +7,7 @@ public class JavaMysql01 {
         System.out.println("hello.git2");
         System.out.println("hello.git3");
         System.out.println("hello.git4");
-        System.out.println("master test");
+        System.out.println("push test");
         System.out.println("hello hotfix");
     }
 }
